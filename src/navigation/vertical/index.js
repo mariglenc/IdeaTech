34117fc5a81081cb1b152d6d72db818c077bea4b
Mariglen Cullhaj <mariglen@twotone.co>
2024-05-27 23:53:47 +0200

    fix lint build error

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -8,31 +8,7 @@ const navigation = () => [
     title: 'Klientet',
     path: '/clients',
     icon: 'ic:baseline-business',
-  },
-  // {
-  //   path: '/sales',
-  //   subject: 'Shitjet',
-  //   title: 'Shitjet',
-  //   icon: 'fa6-brands:sellsy',
-  // },
-  // {
-  //   path: '/softener',
-  //   subject: 'Softener',
-  //   title: 'Softener',
-  //   icon: 'mdi:water-filter',
-  // },
-  // {
-  //   path: '/presentations',
-  //   subject: 'Prezantimet',
-  //   title: 'Prezantimet',
-  //   icon: 'ri:presentation-fill',
-  // },
-  // {
-  //   path: '/agents',
-  //   subject: 'Agjentet',
-  //   title: 'Agjentet',
-  //   icon: 'healthicons:city-worker-outline',
-  // },
+  }
 ]
 
 export default navigation
